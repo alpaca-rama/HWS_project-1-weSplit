72652dab2935351f6fd0761d48faebbd85cb9e3d
Luca <luca@weve-got-it.com>
2022-08-05 15:22:41 +0200

    Initial Commit

---
+++ Project_1_WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project_1_WeSplit
+//
+//  Created by Luca Capriati on 2022/08/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
